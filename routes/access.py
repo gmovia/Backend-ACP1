3b96ba40d38b2083f69622290c5c35e925d637b6
gmovia <gmovia@fi.uba.ar>
2022-10-07 20:01:26 -0300

    agrego casos borde

--- routes/access.py
+++ routes/access.py
@@ -6,12 +6,21 @@ access = APIRouter()
 
 @access.post('/login')
 def login(user: User):
-    user = connection.databaseUsers.users.find_one(dict(user))
-    if(user == None):
+    registeredUser = connection.databaseUsers.users.find_one({"username": user.username})
+    
+    if(registeredUser == None):
         raise HTTPException(status_code=404, detail='The user is not registered.')
+    
+    if(registeredUser["password"] != user.password):
+        raise HTTPException(status_code=404, detail='Incorrect password.')
+    
     return {"detail": 'The user logged in successfully.'}
+
 
 @access.post('/register')
 def register(user: User):
-    connection.databaseUsers.users.insert_one(dict(user))
+    if(connection.databaseUsers.users.find_one({"username": user.username}) != None):
+        raise HTTPException(status_code=404, detail='The user has already been registered.')
+    
+    connection.databaseUsers.users.insert_one({"username": user.username, "password": user.password})
     return {"detail": 'The user is successfully registered.'}
