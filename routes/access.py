448f7057c56f4d9ecd68d840270ae6242a9487b1
RVLareu <rlareu@fi.uba.ar>
2022-10-11 19:06:09 -0300

    endpoints para login, register y user por id

--- routes/access.py
+++ routes/access.py
@@ -1,26 +1,58 @@
-from fastapi import APIRouter, HTTPException
-from models.user import User
-from config.db import connection
+from fastapi import APIRouter, HTTPException, Depends
+from schemas.schemas import User
+from sqlalchemy.orm import Session
+
+from models import  models
+from schemas import schemas
+from config import crud
+from config.db import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
 
 access = APIRouter()
 
-@access.post('/login')
-def login(user: User):
-    registeredUser = connection.databaseUsers.users.find_one({"username": user.username})
-    
-    if(registeredUser == None):
-        raise HTTPException(status_code=404, detail='The user is not registered.')
-    
-    if(registeredUser["password"] != user.password):
-        raise HTTPException(status_code=404, detail='Incorrect password.')
-    
-    return {"detail": 'The user logged in successfully.'}
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
-@access.post('/register')
-def register(user: User):
-    if(connection.databaseUsers.users.find_one({"username": user.username}) != None):
-        raise HTTPException(status_code=404, detail='The user has already been registered.')
+@access.get('/users/{user_id}')
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    """
+    Devuelve un usuario por su id
+    """
+    db_user = crud.get_user(db, user_id = user_id)
+    if db_user is None:
+        raise HTTPException(status_code = 404, detail = "User not found")
+    return db_user
     
-    connection.databaseUsers.users.insert_one({"username": user.username, "password": user.password})
-    return {"detail": 'The user is successfully registered.'}
+
+@access.get('/user')
+def login_user(user: schemas.UserLogin, db: Session = Depends(get_db)):
+    """
+    Devuelve un usuario por su email y contraseña
+    """
+    db_user = crud.get_user_by_email(db, email = user.email)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    if db_user.hashed_password != user.password:
+        raise HTTPException(status_code=404, detail="Incorrect password")
+    return db_user
+
+
+@access.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.User, db: Session = Depends(get_db)):
+    """
+    Crea un usuario
+    """
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
+
+
+
