400adde6d3ee253e8cd27f612dd854951ca25cc6
DHM1996 <dmarkarian@fi.uba.ar>
2022-10-11 20:41:47 -0300

    fix database url

--- config/db.py
+++ config/db.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE_URL')
+SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE_URL').replace("://", "ql://", 1)
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
