0a5fe2f0cfdf3ea393029fd16e07ae02a1897aa9
gmovia <gmovia@fi.uba.ar>
2022-10-13 15:56:07 -0300

    agrego property model

--- models/models.py
+++ models/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Boolean, Column, Integer, String
-
+from sqlalchemy import Boolean, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from config.db import Base
 
 
@@ -9,3 +9,19 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
     password = Column(String)
+    propertys = relationship('Property', backref='user')
+
+class Property(Base):
+    __tablename__ = "propertys"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    direction = Column(String)
+    location = Column(String)
+    province = Column(String)
+    country = Column(String)
+    floors = Column(Integer)
+    rooms = Column(Integer)
+    toilets = Column(Integer)
+    beds = Column(Integer)
+    user_id = Column(Integer, ForeignKey("user.id"))
