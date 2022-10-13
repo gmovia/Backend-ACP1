b19db5daa8983938635eee0d4436d6487b151db6
gmovia <gmovia@fi.uba.ar>
2022-10-13 15:54:21 -0300

    agrego property schema

--- schemas/schemas.py
+++ schemas/schemas.py
@@ -7,3 +7,17 @@ class UserLogin(BaseModel):
 
     class Config:
         orm_mode = True
+
+class PropertySchema(BaseModel):
+    title: str
+    direction: str
+    location: str
+    province: str
+    country: str
+    floors: int
+    rooms: int
+    toilets: int
+    beds: int
+    
+    class Config:
+        orm_mode = True
