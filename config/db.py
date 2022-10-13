d3341d5ee88a762c657b65a22b4367221234a3b3
gmovia <gmovia@fi.uba.ar>
2022-10-13 13:34:59 -0300

    agrego load_dotenv

--- config/db.py
+++ config/db.py
@@ -2,6 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE_URL').replace("://", "ql://", 1)
 
@@ -10,4 +12,4 @@ engine = create_engine(
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base() 
+Base = declarative_base()
