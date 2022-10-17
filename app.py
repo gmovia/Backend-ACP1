b2fadfcf49aa00f7fdc3a7d72623671eb18193d4
RVLareu <rlareu@fi.uba.ar>
2022-10-17 17:48:31 -0300

    agrego cors policy

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
 from fastapi import FastAPI
 from routes.access import access
 from routes.propertyHandler import propertyHandler
+from fastapi.middleware.cors import CORSMiddleware
+
 
 description = """ # API para la gestión de usuarios 👨‍👧‍👧
                 """
+
 
 app = FastAPI(
     description = description,
     title = "Usuarios"
     )
+
+origins = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(access)    
 app.include_router(propertyHandler)
