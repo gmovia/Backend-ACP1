cd1cfe54cc4f7bfbe6f8ee6b4e02f12e84d3f7cc
tomasarrachea <tarrachea@fi.uba.ar>
2022-10-25 12:44:54 -0300

    Set db heroku

--- config/db.py
+++ config/db.py
@@ -2,10 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
+import os
 load_dotenv()
 
-#SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE_URL').replace("://", "ql://", 1)
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE_URL').replace("://", "ql://", 1)
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
@@ -21,5 +22,4 @@ def get_db():
         yield db
     finally:
         db.close()
-
 
