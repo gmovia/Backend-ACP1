c0b00fdf065dcebcb766e7e23a74108130099a08
gmovia <gmovia@fi.uba.ar>
2022-10-28 14:33:52 -0300

    modifico esquema de reserva

--- schemas/reservationSchema.py
+++ schemas/reservationSchema.py
@@ -1,12 +1,11 @@
 from datetime import date
 from pydantic import BaseModel
 
-class ReserveSchema(BaseModel):
-    start_date: str
-    end_date: str
+class ReservationSchema(BaseModel):
+    start_date: date
+    end_date: date
     email_user: str
     publication_id: int
-    price_per_day: int
 
     class Config:
         orm_mode = True
