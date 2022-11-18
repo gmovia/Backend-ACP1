e47fe8c5f25d672abc446d1f561744408508e6cf
gmovia <gmovia@fi.uba.ar>
2022-11-18 12:03:38 -0300

    agrego user email a get review

--- config/reviewCrud.py
+++ config/reviewCrud.py
@@ -1,13 +1,14 @@
 from sqlalchemy.orm import Session
 from models.publication import Publication
 from models.review import Review
+from models.user import User
 from schemas.reviewSchema import ReviewSchema
 
 
 def get_review_by_user(db: Session, db_publication: Publication, user_id: int):
-    return db.query(Review).filter(
+    return db.query(Review, User.email).filter(
         Review.publication_id == db_publication.id).filter(
-            Review.user_id == user_id).first()
+            Review.user_id == user_id).filter(User.id == user_id).first()
 
 
 def put_review(db: Session, db_publication: Publication,
